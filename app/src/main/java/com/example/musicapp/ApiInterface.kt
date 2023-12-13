292839987a603ce32f53f71c6d2abbec74d42be8
Nayan Tripathi <nayantripathi78@gmail.com>
2023-12-13 17:03:46 +0530

    ApiInterface

---
+++ app/src/main/java/com/example/musicapp/ApiInterface.kt
@@ -0,0 +1,15 @@
+package com.example.musicapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @Headers(
+        "X-RapidAPI-Key: 1c4463ea4emsh91e508fa6448c37p1bfeb1jsn65982bb76962",
+        "X-RapidAPI-Host: deezerdevs-deezer.p.rapidapi.com"
+    )
+    @GET("search")
+    fun getData(@Query("q") query: String): Call<MyData>
+}
--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.musicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl("https://deezerdevs-deezer.p.rapidapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getData("eminem")
+
+        retrofitData.enqueue(object : Callback<MyData?> {
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+                Log.d(TAG, "onResponse: ${response.body()}")
+                val dataList = response.body()
+                val textView = findViewById<TextView>(R.id.textView)
+                textView.text = dataList.toString()
+            }
+
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                Log.d(TAG, "onFailure: ${t.message}")
+            }
+        })
+
     }
 }
